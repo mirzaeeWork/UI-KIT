257aa19d9d73c70b51b2063e3dc94ca0bde2454a
samane mirzaee <mirzaeesamane.work@gmail.com>
2025-03-21 15:56:13 +0330

    feat(create base project):add structure main project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CustomButton from "./components/Buttons/CustomButton";
+
+function App() {
+  return (
+    <>
+      <CustomButton>click me</CustomButton>
+    </>
+  );
+}
+
+export default App;
