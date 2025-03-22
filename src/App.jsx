fffdf4ad57cce77b3efb54d6ce5fd7541f071f71
samane mirzaee <mirzaeesamane.work@gmail.com>
2025-03-22 20:10:53 +0330

    feat: add tag for testing auto-update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import CustomButton from "./components/Buttons/CustomButton";
 function App() {
   return (
     <>
+    <h1>My App</h1>
       <CustomButton>click me</CustomButton>
+      
     </>
   );
 }
