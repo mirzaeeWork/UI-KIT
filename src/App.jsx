dd4790c699d24cbbd188281e59e8495c4c25caec
samane mirzaee <mirzaeesamane.work@gmail.com>
2025-03-23 19:24:47 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ButtonUiKit } from "./components";
+
+function App() {
+  return (
+    <>
+      <h1>Samane Mirzaee</h1>
+      <ButtonUiKit>click me</ButtonUiKit>
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  base: '/UI-KIT/',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.jsx'),
+      name: 'UIKit',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
